6a90449616dc79571acb3f254ccea1ed2d26814d
Blakault666 <145710978+BLAKAUT666@users.noreply.github.com>
2025-01-08 11:06:11 -0300

    Add files via upload

---
+++ dominio/ContaBancariaTeste.java
@@ -0,0 +1,15 @@
+package teste;
+
+import dominio.ContaBancaria;
+
+public class ContaBancariaTeste {
+    public static void main(String[] args) {
+        ContaBancaria banco = new ContaBancaria("350600", 1250.53);
+        banco.imprimi();
+
+        // depositar dinheiro
+        banco.depositar(1000);
+        // sacar dinheiro
+        banco.sacar(2800);
+    }
+}
